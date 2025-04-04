ca4f6a2a04a0b90a73f37be908b5b53ff817555e
Kazox6 <pawelka00florian@gmail.com>
2025-04-04 15:32:51 +0200

    Remove MobileDeviceSimulator.

--- addons/MobileControls/Plugin.cs
+++ addons/MobileControls/Plugin.cs
@@ -3,21 +3,8 @@ using Godot;
 
 [Tool]
 public partial class Plugin : EditorPlugin {
-	private Control _dock;
-	
-	public override void _EnterTree() {
-		_dock = GD.Load<PackedScene>(GetPluginPath() + "/MobileDeviceSimulator/MobileDeviceSimulator.tscn").Instantiate<Control>();
-		AddControlToDock(DockSlot.RightBl, _dock);
-	}
+	public override void _EnterTree() { }
 
-	public override void _ExitTree() {
-		RemoveControlFromDocks(_dock);
-		_dock.Free();
-	}
-
-	private string GetPluginPath() {
-		var script = (Script)GetScript();
-		return script.GetPath().GetBaseDir();
-	}
+	public override void _ExitTree() { }
 }
 #endif
