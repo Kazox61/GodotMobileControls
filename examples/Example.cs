d31091ec58a8d9e75ff15278cec8e1fcb1439ac2
Kazox6 <pawelka00florian@gmail.com>
2025-04-07 10:55:32 +0200

    Adds Example.

--- examples/Example.cs
+++ examples/Example.cs
@@ -2,4 +2,22 @@
 
 namespace GodotMobileControls.Examples;
 
-public partial class Example : Control { }
+public partial class Example : Control {
+	[Export] private AnimatedGridContainer _animatedGridContainer;
+	[Export] private StyleBoxMobileButton _styleBoxMobileButton;
+
+	public override void _EnterTree() {
+		_styleBoxMobileButton.TouchPress += OnTouchPress;
+	}
+
+	public override void _ExitTree() {
+		_styleBoxMobileButton.TouchPress -= OnTouchPress;
+	}
+	
+	private void OnTouchPress() {
+		var childCount = _animatedGridContainer.GetChildCount();
+		var randomIndex = GD.RandRange(0, childCount - 1);
+		var randomChild = _animatedGridContainer.GetChild(randomIndex);
+		_animatedGridContainer.RemoveChild(randomChild);
+	}
+}
