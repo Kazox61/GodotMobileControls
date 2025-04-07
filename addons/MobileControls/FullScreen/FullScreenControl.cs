c3fcda9c4c8eae303e2b94db916672baa6071447
Kazox6 <pawelka00florian@gmail.com>
2025-04-07 10:15:56 +0200

    Adds FullScreenControl and SafeAreaExpand.

---
+++ addons/MobileControls/FullScreen/FullScreenControl.cs
@@ -0,0 +1,58 @@
+﻿using Godot;
+
+namespace GodotMobileControls;
+
+[Tool, GlobalClass, Icon("res://addons/MobileControls/Icons/FullScreenControl.svg")]
+public partial class FullScreenControl : Control {
+	public override void _Ready() {
+		if (!IsInsideTree()) {
+			return;
+		}
+
+		var window = GetTree()?.Root;
+		if (window == null) {
+			return;
+		}
+
+		CustomMinimumSize = GetScreenSize(window);
+	}
+
+	public override Vector2 _GetMinimumSize() {
+		if (!IsInsideTree()) {
+			return CustomMinimumSize;
+		}
+
+		var window = GetTree()?.Root;
+		return window != null ? GetScreenSize(window) : CustomMinimumSize;
+	}
+	
+	private Vector2 GetScreenSize(Window window) {
+		if (window == null) {
+			return Vector2.Zero;
+		}
+		
+		Vector2 minimumSize;
+		if (Engine.IsEditorHint()) {
+			var width = ProjectSettings.GetSetting("display/window/size/viewport_width").AsInt32();
+			var height = ProjectSettings.GetSetting("display/window/size/viewport_height").AsInt32();
+			minimumSize = new Vector2(width, height);
+			return minimumSize;
+		}
+		
+		var aspectSize = window.Size.Aspect();
+		var aspectContentScaleSize = window.ContentScaleSize.Aspect();
+		var desiredRes = window.ContentScaleSize;
+
+		minimumSize = Vector2.Zero;
+		if (aspectContentScaleSize < aspectSize) {
+			minimumSize.X = desiredRes.Y * aspectSize;
+			minimumSize.Y = desiredRes.Y;
+		}
+		else {
+			minimumSize.X = desiredRes.X;
+			minimumSize.Y = desiredRes.X / aspectSize;
+		}
+
+		return minimumSize;
+	}
+}
---
+++ addons/MobileControls/SafeArea/SafeAreaExpand.cs
@@ -0,0 +1,36 @@
+﻿using Godot;
+
+namespace GodotMobileControls;
+
+[GlobalClass, Icon("res://addons/MobileControls/Icons/SafeAreaContainer.svg")]
+public partial class SafeAreaExpand : Control {
+	public enum ExpandPositionEnum {
+		Left,
+		Top,
+		Right,
+		Bottom
+	}
+
+	[Export] public ExpandPositionEnum ExpandPosition = ExpandPositionEnum.Top;
+
+	public override void _Ready() {
+		CustomMinimumSize = GetMinSize();
+	}
+
+	public override Vector2 _GetMinimumSize() {
+		return GetMinSize();
+	}
+
+	private Vector2 GetMinSize() {
+		var safeArea = DisplayServer.GetDisplaySafeArea();
+		var screenSize = DisplayServer.ScreenGetSize();
+
+		return ExpandPosition switch {
+			ExpandPositionEnum.Top => new Vector2(0, safeArea.Position.Y),
+			ExpandPositionEnum.Left => new Vector2(safeArea.Position.X, 0),
+			ExpandPositionEnum.Bottom => new Vector2(0, screenSize.Y - safeArea.End.Y),
+			ExpandPositionEnum.Right => new Vector2(screenSize.X - safeArea.End.X, 0),
+			_ => Vector2.Zero
+		};
+	}
+}
