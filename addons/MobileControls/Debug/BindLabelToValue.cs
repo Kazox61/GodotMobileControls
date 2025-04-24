b01114994fae8dc525d3f2ed98aa99895a1aa010
Kazox6 <pawelka00florian@gmail.com>
2025-04-24 12:56:07 +0200

    Adds Debug Elements.

---
+++ addons/MobileControls/Debug/BindLabelToValue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Reflection;
+using Godot;
+
+namespace GodotMobileControls.Debug;
+
+[GlobalClass]
+public partial class BindLabelToValue : Node {
+	[Export] private Label _label;
+	[Export] private Node _target;
+	[Export] private string _targetPath;
+
+	public override void _Process(double delta) {
+		object value = default;
+		try {
+			value = GetPropertyValue(_target, _targetPath);
+		}
+		catch (Exception) {
+			// ignored
+		}
+
+		try {
+			value = GetFieldValue(_target, _targetPath);
+		}
+		catch (Exception) {
+			// ignored
+		}
+
+		if (value == null) {
+			return;
+		}
+
+		_label.Text = $"{_targetPath}: {value}";
+	}
+
+	private static object GetPropertyValue(object obj, string propertyName) {
+		ArgumentNullException.ThrowIfNull(obj);
+
+		var type = obj.GetType();
+		var propertyInfo = type.GetProperty(propertyName);
+
+		if (propertyInfo == null) {
+			throw new ArgumentException($"Property '{propertyName}' not found on type '{type.FullName}'");
+		}
+
+		return propertyInfo.GetValue(obj);
+	}
+
+
+	private static object GetFieldValue(object obj, string fieldName) {
+		ArgumentNullException.ThrowIfNull(obj);
+
+		var type = obj.GetType();
+		var fieldInfo =
+			type.GetField(fieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+		if (fieldInfo == null) {
+			throw new ArgumentException($"Field '{fieldName}' not found on type '{type.FullName}'");
+		}
+
+		return fieldInfo.GetValue(obj);
+	}
+}
---
+++ addons/MobileControls/Debug/WorldLabel.cs
@@ -0,0 +1,12 @@
+﻿using Godot;
+
+namespace GodotMobileControls.Debug;
+
+[GlobalClass]
+public partial class WorldToScreen : Node {
+	[Export] private Node3D _anchor;
+	
+	public override void _Process(double delta) {
+		GetParent<Control>().GlobalPosition = GetViewport().GetCamera3D().UnprojectPosition(_anchor.Position);
+	}
+}
